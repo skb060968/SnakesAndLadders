d8591bc36b965ac1b0fb5fb7fe8ca1c98e13cbc5
skb060968 <skb060968@gmail.com>
2025-12-21 15:29:48 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = "snl-3d-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./manifest.json",
+
+  "./images/die-1.png",
+  "./images/die-2.png",
+  "./images/die-3.png",
+  "./images/die-4.png",
+  "./images/die-5.png",
+  "./images/die-6.png",
+
+  "./images/board.png",
+  "./images/red.png",
+  "./images/green.png",
+  "./images/wood.png",
+  "./images/frame-wood.png",
+
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+
+  "./sounds/dice-roll.mp3",
+  "./sounds/move.mp3",
+  "./sounds/snake.mp3",
+  "./sounds/ladder.mp3",
+  "./sounds/win.mp3"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => (key !== CACHE_NAME ? caches.delete(key) : null))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached ||
+      fetch(event.request).catch(() => caches.match("./index.html"))
+    )
+  );
+});
