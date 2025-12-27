ab9c48f28dc6b5759d05433b4d9130f24b553c16
Sunil <skb060968@gmail.com>
2025-12-27 13:57:34 +0530

    Locked PWA

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first + update prompt
 ============================== */
 
-const CACHE_NAME = "snl-3d-v19"; // bump on every deploy
+const CACHE_NAME = "snl-3d-v20"; // bump on every deploy
 
 const ASSETS = [
   "./",
